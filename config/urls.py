d98b6e99040bfc2ee8da1352fbf18be2d7556774
unknown <abdusamaddev571@gmail.com>
2023-06-06 13:30:13 +0500

    commit 07

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,10 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from blogs.views import home
 
 urlpatterns = [
-                  path("", home, name="home"),
-                  path('admin/', admin.site.urls),
-                  path("blogs/", include("blogs.urls")),
-              ]
+    path("", home, name="home"),
+    path('admin/', admin.site.urls),
+    path("blogs/", include("blogs.urls")),
+]
